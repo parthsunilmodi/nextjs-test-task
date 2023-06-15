86dce2de3b51894e5b4d0617eb27394737b68f9e
Parth Modi <parthmodi.developer@gmail.com>
2023-06-15 15:06:04 +0530

    feat: persisted data in cart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,12 @@
 import React from 'react';
 import NavBar from '../app/components/nav-bar';
 import ProductList from '../app/components/products/index';
-import WithAuth from '../components/Auth'
-
+import WithAuth from '../components/Auth';
 
 const Home = () => {
   return (
-    <WithAuth>
-      <NavBar/>
+    <WithAuth isProtected={true}>
+      <NavBar />
       <div className="h-screen bg-[#dfe3ee]">
         <ProductList />
       </div>
